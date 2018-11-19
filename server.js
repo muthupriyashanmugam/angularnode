de72dbd7ac2792ac7c6a8595bad62aca752613be
muthupriya <muthupriya.shanmugam@ameexusa.com>
2018-11-19 15:08:11 +0530

    updated the package json with angular modules

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.get('/', (req, res) => res.sendFile( __dirname + "/" + "index.html" ))
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
